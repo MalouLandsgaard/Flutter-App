0ff824b7da5af93c0d83e008a983b2d14db54f61
Malou <elmelund@hotmail.dk>
2022-06-01 12:12:54 +0200

    New app

---
+++ lib/my_home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/home.dart';
+import 'package:flutter_app/menu.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(children: [Menu(), HomeView()]),
+      ),
+    );
+  }
+}
